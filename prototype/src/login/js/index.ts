0bb12120f6b87e141b016e7da8814de0fcda1e8e
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-10-29 03:40:45 +0100

    Add js email & password validation

--- prototype/src/login/js/index.ts
+++ prototype/src/login/js/index.ts
@@ -7,15 +7,32 @@ type LoginResponse = LoginFailedResponse | {
   success: true
 };
 
+type Credentials = {
+  email: string
+  password: string
+};
+
 $(() => {
   $('#login-form').on('submit', function (e) {
     e.preventDefault();
     const $this = $(this);
 
-    console.log("LOGIN:");
+    const credentials = Object.fromEntries(new FormData(this as HTMLFormElement)) as Credentials;
+    console.log(credentials);
+
+    const { email, password } = credentials;
+
     console.log($this.serialize());
 
-    // TODO: validate email & pw
+    if (!isValidWorkEmail(email)) {
+      alert('Invalid email!');
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      alert('Invalid password!');
+      return;
+    }
 
     $('#login-btn').prop('disabled', true);
 
@@ -27,18 +44,20 @@ $(() => {
     })
       .done((data: LoginResponse) => {
         if (data.success) {
-          // TODO: go to main page
+          // TODO: store email in localStorage? so other pages can access it
+          // localStorage.setItem('email', credentials.email);
+
           console.log(data);
           alert(JSON.stringify(data));
 
-          window.location.href = "/main";
+          redirect('todo');
         } else {
-          // TODO: alert that incorrect password/email/whatever
+          // TODO: notify that incorrect password/email/whatever (not an alert, some sort of like tooltip)
           alert(`ERROR: ${data.errorMessage}`);
         }
       })
-      .fail(() => {
-        alert("ERROR: request failed");
+      .fail((xhr, errorTextStatus, errorMessage) => {
+        alert(`${errorTextStatus}: ${errorMessage}`);
       })
       .always(() => {
         $('#login-btn').prop('disabled', false);
@@ -47,3 +66,20 @@ $(() => {
 
   (<any>$('.multiline-tooltip')).tooltip({ html: true });
 });
+
+function redirect(url: string) {
+  window.location.href = url;
+}
+
+function isValidWorkEmail(email: string): boolean {
+  return email.endsWith('@make-it-all.co.uk') && email !== '@make-it-all.co.uk';
+}
+
+const PASSWORD_REGEX = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+
+function isValidPassword(password: string): boolean {
+  if (password.length < 12)
+    return false;
+
+  return PASSWORD_REGEX.test(password);
+}
