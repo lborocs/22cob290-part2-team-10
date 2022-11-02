0f178f9140a43de4fb066251c0eb48ae4da325ae
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-02 21:22:38 +0000

    Fix importing JS

--- prototype/src/home.php
+++ prototype/src/home.php
@@ -248,7 +248,7 @@ $email = $_REQUEST['email'];
       integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm"
       crossorigin="anonymous"></script>
 
-    <script src="home/script.js"></script>
+    <script type="module" src="home/script.js"></script>
 </body>
 
 </html>
