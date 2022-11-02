e0c8d0db3eeefd1f7b95dd116903b7b0945017e8
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-02 00:12:51 +0000

    Update kboard in todo

--- prototype/src/todo/script.js
+++ prototype/src/todo/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     });
 });
 /*ADDED CODE FROM KBOARD*/
-/*By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element. -w3schools*/
+/* By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element. -w3schools*/
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -13,112 +13,113 @@ function drag(ev) {
 }
 
 function drop(ev) {
-	var _target = $("#" + ev.target.id);
-	var data = ev.dataTransfer.getData("text");
-	if ($(_target).hasClass("noDrop")) {
-		console.log("no transfer");
-		ev.preventDefault();
-	} else {
-		ev.preventDefault();
-		ev.target.appendChild(document.getElementById(data));
-	}
+  const _target = $(ev.target);
+  const data = ev.dataTransfer.getData("text");
+
+  ev.preventDefault();
+  if (_target.hasClass("tasks")) {
+    ev.target.appendChild(document.getElementById(data));
+  } else {
+    console.log("no transfer");
+  }
 }
 
-$(function() {
-	/* TRY TO MAKE MORE EFFICIENT USING TECHNIQUE LIKE 
-	var span = document.getElementsByClassName("close")[0];*/
-	
-    // Get the modal
-	var modal = $("#modal");
-	
-	// Get the buttons that opens the modals
-	var to_do_btn = $("#to_do_button");
-	var in_progress_button = $("#in_progress_button");
-	var code_review_button = $("#code_review_button");
-	var completed_button = $("#completed_button");
-	
-	// All buttons for modal
-	var close_modal_button1 = $("#close_modal1");
-	var close_modal_button2 = $("#close_modal2");
-	var add_task_modal_button = $("#add_task");
-	
-	var taskno = 2;
-	
-	function open_modal(id){
-		add_task_modal_button.prop("onclick", null).off("click");
-		add_task_modal_button.on('click', () => add_task(id));
-		
-		modal.css('display',"block");
-		
-	}
-	
-	function close_modal(){
-		modal.css('display',"none");
-	}
-	
-	function process_tags(tags) {
-		var tagsArr=tags.split(",");
-		var output = "";
-		
-		for (tag of tagsArr) {
-			output = output.concat(`<span class='badge bg-primary text-white mb-2 mx-1'>${tag}</span>`)
-		}
-		
-		return output;
-	}
-	
-	function validate_form(form_data) {
-		if (form_data.title == "") {
-			alert("Title must be filled out");
-			return false;
-		} else if (form_data.description == ""){
-			alert("Task needs a description");
-			return false;
-		}
-			
-		return true;
-	}
-	
-	function add_task(id) {
-		
-		var task_form = $('#task_info')[0];
-		
-		const form_data = Object.fromEntries(new FormData(task_form));
-		const { title, description, tags } = form_data;
-		
-		if (!validate_form(form_data)) {
-			return;
-		}
-			
-		// handle form
-		$(`#${id}`).append(
-		`<div class="card mb-3 drag_item noDrop" id="task`+ taskno +`" draggable="true" ondragstart="drag(event)" >
-		  <div class="card-body">
-			<button type="button" class="close" aria-label="Close" onclick="this.parentNode.parentNode.parentNode.removeChild(parentNode.parentNode);">
-			<span aria-hidden="true">&times;</span></button><br>
-			${process_tags(tags)}
-			<p style="font-weight: bold">${title}</p>
-			<p class="mb-0">${description}</p>
-		  </div>
-		</div>`
-		);
-		
-		task_form.reset();
-		console.log(taskno);
+$(function () {
+  /* TRY TO MAKE MORE EFFICIENT USING TECHNIQUE LIKE
+  var span = document.getElementsByClassName("close")[0];*/
 
-		taskno++;
-		
-		close_modal();
-	}
-	
-	$('.open_modal').on('click', function (e) {
-		const $this = $(this);
-		
-		const target_id = $this.attr('data-id');
-		console.log(target_id);
-		open_modal(target_id);
-	});
+  // Get the modal
+  const modal = $("#modal");
 
-	close_modal_button1.on('click', close_modal);
-	close_modal_button2.on('click', close_modal);
+  // All buttons for modal
+  const close_modal_button1 = $("#close_modal1");
+  const close_modal_button2 = $("#close_modal2");
+  const add_task_modal_button = $("#add_task");
+
+  let taskno = 2;
+
+  function open_modal(id) {
+    // remove previous onclick listener: https://stackoverflow.com/a/1687805
+    add_task_modal_button.prop("onclick", null).off("click");
+
+    add_task_modal_button.on("click", () => add_task(id));
+
+    show_modal();
+  }
+
+  function show_modal() {
+    modal.css("display", "block");
+  }
+
+  function close_modal() {
+    modal.css("display", "none");
+  }
+
+  function process_tags(tags) {
+    return tags.split(",")
+      .map((tag) => `<span class='badge bg-primary text-white mb-2 mx-1'>${tag.trim()}</span>`)
+      .join("");
+  }
+
+  function validate_form(form_data) {
+    if (form_data.title === "") {
+      alert("Title must be filled out");
+      return false;
+    } else if (form_data.description === "") {
+      alert("Task needs a description");
+      return false;
+    }
+
+    return true;
+  }
+
+  function add_task(task_container_id) {
+    const task_form = $("#task_info")[0];
+
+    const form_data = Object.fromEntries(new FormData(task_form));
+    const { title, description, tags } = form_data;
+
+    if (!validate_form(form_data)) {
+      return;
+    }
+
+    $(`#${task_container_id}`).append(`
+    <div class="card mb-3 drag_item" id="task${taskno}" draggable="true" ondragstart="drag(event)" >
+      <div class="card-body">
+        <button type="button" class="close" aria-label="Close" onclick="remove_task(this)">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <br>
+        ${process_tags(tags)}
+        <p style="font-weight: bold">${title}</p>
+        <p class="mb-0">${description}</p>
+      </div>
+    </div>
+    `);
+
+    task_form.reset();
+    console.log(taskno);
+
+    taskno++;
+
+    close_modal();
+  }
+
+  $(".open_modal").on("click", function (e) {
+    const $this = $(this);
+
+    const task_container_id = $this.attr("data-id");
+    open_modal(task_container_id);
+  });
+
+  close_modal_button1.on("click", close_modal);
+  close_modal_button2.on("click", close_modal);
 });
+
+function remove_task(button) {
+  const $button = $(button);
+  console.log(button);
+
+  const task_card = $button.parent().parent();
+  task_card.remove();
+}
