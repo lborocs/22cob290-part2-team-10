5e8ec5506eecb74f91d98f1e8d75f551203a2c5a
Luyando Lisimba <luyandolisimba@yahoo.co.uk>
2022-11-03 18:18:53 +0000

    corrected staff_overview.php file from last commit

--- prototype/src/manager_staff_overview/staff_overview.php
+++ prototype/src/manager_staff_overview/staff_overview.php
@@ -4,79 +4,66 @@
   <head>
     <!--BOOTSTRAP CDN-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!--JQUERY CDN-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-	<script src="staff_overview.js"></script>
-  <link rel="stylesheet" href="style.css">
+  </head>
 
   <body>
-    <div class="container">
 
-
-	  <h1>
-	    Staff Overview
-	  </h1>
-
-
-
-	  <div>
-	    <ul class="list-group">
-		  <!-- To do:
-		  - onclick update employee role on database
-		  - onclick delete/update employee details-->
-          <li class="list-group-item employeelist">Employee 1
-		    <span style="float: right">
-			  <select class="form-select">
-                <option>Role 1</option>
-			    <option>Role 2</option>
-			    <option>Role 3</option>
-			    <option>Role 4</option>
-				<option style="background-color: #FF0000;">DELETE EMPLOYEE</option>
-			  </select>
-			</span>
-		  </li>
-          <li class="list-group-item employeelist">Employee 2
-		    <span style="float: right">
-			  <select class="form-select">
-                <option>Role 1</option>
-			    <option>Role 2</option>
-			    <option>Role 3</option>
-			    <option>Role 4</option>
-				<option style="background-color: #FF0000;">DELETE EMPLOYEE</option>
-			  </select>
-			</span>
-		  </li>
-          <li class="list-group-item employeelist">Employee 3
-		    <span style="float: right">
-			  <select class="form-select">
-                <option>Role 1</option>
-			    <option>Role 2</option>
-			    <option>Role 3</option>
-			    <option>Role 4</option>
-				<option style="background-color: #FF0000;">DELETE EMPLOYEE</option>
-			  </select>
-			</span>
-		  </li>
-          <li class="list-group-item employeelist">Employee 4
-		    <span style="float: right">
-			  <select class="form-select">
-                <option>Role 1</option>
-			    <option>Role 2</option>
-			    <option>Role 3</option>
-			    <option>Role 4</option>
-				<option style="background-color: #FF0000;">DELETE EMPLOYEE</option>
-			  </select>
-			</span>
-		  </li>
-        </ul>
-	  </div>
-	  <br>
-	  <button type="button" class="btn btn-primary">Add employee</button>
-	  <button type="button" class="btn btn-success">Save changes</button>
-
+  <div id="modal" class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+	  <div class="modal-content">
+	    <div class="modal-header">
+		  <h5 class="modal-title">Add employee</h5>
+			<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close" onclick="close_modal()">
+			<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body">
+			<!--POP-UP FORM-->
+		  <form role="form" method="POST" id="emp_info" action="">
+			<div class="form-group">
+			  <label  class="control-label">Employee:</label>
+			  <div>
+			    <select class="form-select" id="employees" required>
+			    </select>
+			  </div>
+			</div>
+			<div class="form-group">
+			  <label class="control-label">Role:</label>
+		      <div>
+			    <select  class="form-select" id="roles" required>
+			    </select>
+			  </div>
+			</div>
+		  </form>
+		</div>
+		<div class="modal-footer">
+		  <button type="button" class="btn btn-secondary" onclick="close_modal()">Close</button>
+	      <button type="button" class="btn btn-primary" id="add_employee_btn">Add Employee</button>
+		</div>
 	  </div>
 	</div>
+  </div>
 
+
+  <div class="container">
+    <h1>
+	  Staff Overview
+    </h1>
+    <div>
+	  <ul class="list-group" id="staff_list">
+	  </ul>
+    </div>
+    <br>
+    <button type="button" class="btn btn-primary" onclick="open_modal()">Assign employee</button>
+    <button type="button" class="btn btn-success" id="save_changes_btn">Save changes</button>
+
+  </div>
+</div>
+
+
+     <!--JQUERY CDN-->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="staffoverview.js"></script>
 
   </body>
 
