f502ac5a46b29726b7fd4c2ec18e09b30496b7a4
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-06 02:55:15 +0000

    Hide redirect form

--- prototype/src/utils/redirect/index.js
+++ prototype/src/utils/redirect/index.js
@@ -1,7 +1,7 @@
 // redirect with data: https://stackoverflow.com/a/10022098
 export default function redirect(url, data) {
     const $form = $(`
-  <form action="${url}" method="POST">
+  <form action="${url}" method="POST" class="d-none">
     ${Object.entries(data).map(([key, value]) => `<input name="${key}" value='${JSON.stringify(value)}' />`).join('')}
   </form>
   `);
