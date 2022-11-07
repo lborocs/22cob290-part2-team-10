ccb2a40e985c688e0edcedd691bc03e63527b999
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-07 15:20:12 +0000

    Convert staffoverview to TS & use correct user roles

--- prototype/src/manager_staff_assignment/staffoverview.js
+++ prototype/src/manager_staff_assignment/staffoverview.js
@@ -1,89 +1,70 @@
 import '../utils/redirect';
-
 const modal = $("#modal");
 const staff_list = $("#staff_list");
-
 const active_employees = [
-  {
-    name: "John Gatsby",
-    role: "Team Member",
-  },
+    {
+        name: "John Gatsby",
+        role: "Team Member",
+    },
 ];
-
 const employee_list = [
-  "Loki Dillon",
-  "Myrtle Blake",
-  "Lennox O'Moore",
-  "Danika Sellers",
-  "Alberto Dunkley",
-  "Ross Kouma",
-  "Perry Macgregor",
-  "Demi-Lee Hawes",
-  "Keagan Mccormick",
-  "Guto Guest",
+    "Loki Dillon",
+    "Myrtle Blake",
+    "Lennox O'Moore",
+    "Danika Sellers",
+    "Alberto Dunkley",
+    "Ross Kouma",
+    "Perry Macgregor",
+    "Demi-Lee Hawes",
+    "Keagan Mccormick",
+    "Guto Guest",
 ];
 const roles = [
-  "Team Leader",
-  "Team Member",
-  "Analyst",
-  "Consultant",
+    "Manager",
+    "Team Leader",
+    "Team Member",
 ];
-
 // set default option to its current role so that when u click save changes u dont have to change every role from the first one in list
 function get_roles_as_select_menu(selected_role) {
-  let str = "";
-
-  for (const role of roles) {
-    const selected = selected_role === role;
-
-    str += `<option ${selected ? 'selected' : ''} >${role}</option>`;
-  }
-
-  return str;
+    let str = "";
+    for (const role of roles) {
+        const selected = selected_role === role;
+        str += `<option ${selected ? 'selected' : ''} >${role}</option>`;
+    }
+    return str;
 }
-
+// @ts-ignore
 window.open_modal = function open_modal() {
-  update_employees();
-  modal.css('display', "block");
+    update_employees();
+    modal.css('display', "block");
+};
+function close_modal() {
+    modal.css('display', "none");
 }
-
-window.close_modal = function close_modal() {
-  modal.css('display', "none");
-}
-
-$(function () {
-
-});
-
+// @ts-ignore
+window.close_modal = close_modal;
 // this will be database oriented later
 function update_employees() {
-
-  const employee_select = $("#employees");
-  const role_select = $("#roles");
-
-  employee_select.empty();
-
-  for (const emp_name of employee_list) {
-    // prevent same employee being added twice
-    const index = active_employees.findIndex(employee => employee.name === emp_name);
-    if (index === -1) {
-      employee_select.append(`<option>${emp_name}</option>`);
+    const employee_select = $("#employees");
+    const role_select = $("#roles");
+    employee_select.empty();
+    for (const emp_name of employee_list) {
+        // prevent same employee being added twice
+        const index = active_employees.findIndex(employee => employee.name === emp_name);
+        if (index === -1) {
+            employee_select.append(`<option>${emp_name}</option>`);
+        }
     }
-  }
-
-  role_select.empty();
-  for (let i = 0; i < roles.length; i++) {
-    role_select.append(`<option>${roles[i]}</option>`);
-  }
+    role_select.empty();
+    for (let i = 0; i < roles.length; i++) {
+        role_select.append(`<option>${roles[i]}</option>`);
+    }
 }
-
 function add_employee(eName, eRole) {
-  //console.log(get_roles_as_select_menu());
-  const emp_form = $("#emp_info")[0];
-
-  active_employees.push({ name: eName, role: eRole });
-
-  staff_list.append(`
+    //console.log(get_roles_as_select_menu());
+    const emp_form = $("#emp_info")[0];
+    active_employees.push({ name: eName, role: eRole });
+    staff_list.append(`
   <li class="list-group-item employee_list_item">
     <span class="employee_list_item_name">${eName}</span>
     <span class="employee_list_item_role" style="float: right">${eRole}</span>
@@ -93,55 +74,47 @@ function add_employee(eName, eRole) {
     </select>
   </li>
   `);
-
-  update_employees();
-  /*
-  console.log(active_employees);
-  */
+    update_employees();
+    /*
+    console.log(active_employees);
+    */
 }
-
 function update_roles() {
-  const employee_list_items = $(".employee_list_item");
-  employee_list_items.each(function () {
-    const $this = $(this);
-
-    const ename = $this.find(".employee_list_item_name").text();
-
-    const role = $this.find("option:selected").text();
-
-    if (role === "DELETE EMPLOYEE") {
-      $this.remove();
-      for (let i = 0; i < active_employees.length; i++) {
-        if (active_employees[i].name === ename) {
-          active_employees.splice(i, 1);
-          console.log(active_employees);
+    const employee_list_items = $(".employee_list_item");
+    employee_list_items.each(function () {
+        const $this = $(this);
+        const ename = $this.find(".employee_list_item_name").text();
+        const role = $this.find("option:selected").text();
+        if (role === "DELETE EMPLOYEE") {
+            $this.remove();
+            for (let i = 0; i < active_employees.length; i++) {
+                if (active_employees[i].name === ename) {
+                    active_employees.splice(i, 1);
+                    console.log(active_employees);
+                }
+            }
+            console.log(ename);
+            //console.log(active_employees);
+            update_employees();
         }
-      }
-      console.log(ename);
-      //console.log(active_employees);
-      update_employees();
-    } else {
-      $this.find(".employee_list_item_role").text(role);
-    }
-  });
+        else {
+            $this.find(".employee_list_item_role").text(role);
+        }
+    });
 }
-
-
 $('#add_employee_btn').on('click', function (e) {
-  const eName = $("#employees :selected").text();
-  const eRole = $("#roles :selected").text();
-  add_employee(eName, eRole);
-  close_modal();
+    const eName = $("#employees :selected").text();
+    const eRole = $("#roles :selected").text();
+    add_employee(eName, eRole);
+    close_modal();
 });
-
 $('#save_changes_btn').on('click', function (e) {
-  update_roles();
+    update_roles();
 });
-
 /*
 
 for all elements with class name:
-	check value of span
+    check value of span
 
 
 */
