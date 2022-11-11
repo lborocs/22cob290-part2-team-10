ade590dd7446d96a3f761b910c03412c79116403
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-11 03:33:36 +0000

    Set 400 response code if not all params set

--- prototype/src/php/params.php
+++ prototype/src/php/params.php
@@ -4,6 +4,7 @@ function require_and_unpack_params(array $params): void
 {
   foreach ($params as $param => &$variable) {
     if (!isset($_REQUEST[$param])) {
+      http_response_code(300);
       error('Not all params set.');
     }
 
