d3d383c293e56513e7a807b4c824ccbbdf354c88
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-18 00:48:47 +0000

    Conform to ESLint rules

--- prototype_nextjs/src/pages/index.tsx
+++ prototype_nextjs/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 import Okay from 'components/Okay';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
