b7881705faa0a04586f979a79384825a447d6f27
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-20 07:37:40 +0000

    Work on profile

--- next13/src/pages/profile.tsx
+++ next13/src/pages/profile.tsx
@@ -1,8 +1,13 @@
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
+import Head from 'next/head';
 import { unstable_getServerSession } from 'next-auth/next';
-// import { useSession } from 'next-auth/react';
 import { signOut } from 'next-auth/react';
+
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
 
 import { authOptions } from '~/pages/api/auth/[...nextauth]';
 import { getUser } from '~/pages/api/user/getUser';
@@ -10,21 +15,87 @@ import { getUser } from '~/pages/api/user/getUser';
 export default function ProfilePage({ user }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   if (!user) return null;
 
-  const { email, role } = user;
+  const { fname, lname, email, role } = user;
+
+  // TODO: text avatar on the left or smthn, and click on it to open edit modal
+
+  // TODO: name editable
 
   return (
-    <main>
-      <div>
-        Email: {email}
-      </div>
-      <div>
-        Role: {role}
-      </div>
+    <>
+      <Head>
+        <title>Profile - Make-ItAll</title>
+      </Head>
+      <main>
+        <section>
+          <Row>
+            <Form.Group as={Col} lg={5}>
+              <InputGroup>
+                <InputGroup.Text>Name</InputGroup.Text>
+                <Form.Control
+                  defaultValue={fname}
+                />
+                <Form.Control
+                  defaultValue={lname}
+                />
+              </InputGroup>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Col>
+              <h3>Email</h3>
+              <span>{email}</span>
+            </Col>
+            <Col>
+              <h3>Role</h3>
+              <span>{role}</span>
+            </Col>
+          </Row>
+        </section>
 
-      <Button variant='danger' onClick={() => signOut({ callbackUrl: '/' })}>
-        Sign Out
-      </Button>
-    </main>
+        <br />
+        <br />
+
+        <section>
+          <Row>
+            <Col>
+              <h3>Change Password</h3>
+              <Button variant='dark'>
+                Change
+              </Button>
+
+              {/* TODO: modal & toast */}
+            </Col>
+            <Col>
+              <h3>Invite Employee</h3>
+              <Button variant='dark'>
+                Generate invite
+              </Button>
+
+              {/* TODO: modal */}
+            </Col>
+          </Row>
+        </section>
+
+        <br />
+        <br />
+
+        <section>
+          <h3>Avatar</h3>
+          <Button variant='dark'>
+            Change avatar colours
+          </Button>
+          {/*  TODO: modal */}
+        </section>
+
+        <br />
+        <br />
+
+        <Button variant='danger' onClick={() => signOut({ callbackUrl: '/' })}>
+          Sign Out
+        </Button>
+      </main>
+    </>
   );
 }
 
