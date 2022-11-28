84814f3d526b65288fc43593cc3e9a619c89d7e0
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-28 12:53:12 +0000

    Update default files

--- prototype_nextjs/src/pages/nextjs.tsx
+++ prototype_nextjs/src/pages/nextjs.tsx
@@ -3,7 +3,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-import styles from '~/styles/Home.module.css';
+import styles from '~/styles/nextjs.module.css';
 
 export default function Home() {
   return (
