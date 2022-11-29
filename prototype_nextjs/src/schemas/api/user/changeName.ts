204cf4410b3cca45c97709e6560e607afeb3801e
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-11-29 03:36:42 +0000

    Update ChangeNameSchema to only allow alphabetic characters

--- prototype_nextjs/src/schemas/api/user/changeName.ts
+++ prototype_nextjs/src/schemas/api/user/changeName.ts
@@ -1,8 +1,13 @@
 import { z } from 'zod';
 
+// name regex: https://stackoverflow.com/a/36733683
 const ChangeNameSchema = z.object({
-  firstName: z.string().trim().min(1, 'First name is required'),
-  lastName: z.string().trim().min(1, 'Last name is required'),
+  firstName: z.string()
+    .regex(/^[a-z]+$/i, 'First name is alphabetic only')
+    .min(1, 'First name is required'),
+  lastName: z.string()
+    .regex(/^[a-z]+$/i, 'Last name is alphabetic only')
+    .min(1, 'Last name is required'),
 });
 
 export default ChangeNameSchema;
