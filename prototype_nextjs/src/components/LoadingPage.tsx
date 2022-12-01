1d2bbe48d810766df47d12111ee89ddb3ccfd11f
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-01 07:29:35 +0000

    Make LoadingPage more flexible

--- prototype_nextjs/src/components/LoadingPage.tsx
+++ prototype_nextjs/src/components/LoadingPage.tsx
@@ -1,18 +1,20 @@
 import Spinner from 'react-bootstrap/Spinner';
 
-export default function LoadingPage() {
+export type LoadingPageProps = {
+  dark?: boolean
+};
+
+export default function LoadingPage({ dark = true }: LoadingPageProps) {
   return (
-    <main>
-      <div className="vh-100 vw-100 d-flex justify-content-center align-items-center bg-dark">
-        <Spinner
-          animation="border"
-          variant="light"
-          role="status"
-          size="sm"
-        >
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </div>
-    </main>
+    <div className={`flex-grow h-100 w-100 d-flex justify-content-center align-items-center ${dark ? 'bg-dark' : ''}`}>
+      <Spinner
+        animation="border"
+        variant={dark ? 'light' : undefined}
+        role="status"
+        size="sm"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </div>
   );
 }
