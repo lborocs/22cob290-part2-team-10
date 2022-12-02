c5e953f5efa80d900e3534532f91136f5d452a95
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-02 02:42:21 +0000

    Add links to NewProject and ProjectOverview pages

--- prototype_nextjs/src/pages/dashboard.tsx
+++ prototype_nextjs/src/pages/dashboard.tsx
@@ -1,13 +1,15 @@
 import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { unstable_getServerSession } from 'next-auth/next';
 
 import { SidebarType, type PageLayout } from '~/components/Layout';
 import { authOptions } from '~/pages/api/auth/[...nextauth]';
 import { ssrGetUserInfo } from '~/server/utils';
+import { getAssignedProjects } from '~/server/store/projects';
 
 // TODO: DashboardPage
-export default function DashboardPage({ }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function DashboardPage({ projects }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
   return (
     <main>
@@ -15,6 +17,25 @@ export default function DashboardPage({ }: InferGetServerSidePropsType<typeof ge
         <title>Dashboard - Make-It-All</title>
       </Head>
       {/* TODO */}
+      <h1>Manager/team leader dashboard</h1>
+
+      <div>
+        <ul>
+          {projects.map((project, index) => (
+            <Link key={index} href={`/projects/${project.id}/overview`}>
+              <li>
+                {project.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <Link href="projects/new">
+        <button>
+          New project
+        </button>
+      </Link>
     </main>
   );
 }
@@ -28,10 +49,14 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   const user = await ssrGetUserInfo(session);
 
+  // TODO: create new func getManagedProjects or something like that
+  const projects = await getAssignedProjects(user.email);
+
   return {
     props: {
       session,
       user,
+      projects,
     },
   };
 }
