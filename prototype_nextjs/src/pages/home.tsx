bb54e0363261ba7540c80f1bbe327f067917e550
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-02 02:02:57 +0000

    Use new layout in Home

--- prototype_nextjs/src/pages/home.tsx
+++ prototype_nextjs/src/pages/home.tsx
@@ -2,7 +2,7 @@ import type { GetServerSidePropsContext, InferGetServerSidePropsType } from 'nex
 import Head from 'next/head';
 import { unstable_getServerSession } from 'next-auth/next';
 
-import Layout from '~/components/Layout';
+import { SidebarType, type PageLayout } from '~/components/Layout';
 import { authOptions } from '~/pages/api/auth/[...nextauth]';
 import { ssrGetUserInfo } from '~/server/utils';
 
@@ -10,16 +10,12 @@ import { ssrGetUserInfo } from '~/server/utils';
 export default function HomePage({ }: InferGetServerSidePropsType<typeof getServerSideProps>) {
 
   return (
-    <>
+    <main>
       <Head>
         <title>Home - Make-It-All</title>
       </Head>
-      <Layout sidebarType="projects">
-        <main>
-          {/* TODO */}
-        </main>
-      </Layout>
-    </>
+      {/* TODO */}
+    </main>
   );
 }
 
@@ -39,3 +35,10 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     },
   };
 }
+
+const layout: PageLayout = {
+  sidebar: {
+    type: SidebarType.PROJECTS,
+  },
+};
+HomePage.layout = layout;
