7f552ab2974b3ac5130f4b60111d6ce168dc2ed4
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-03 05:50:31 +0000

    Add project data to seed

--- prototype_nextjs/prisma/seed.ts
+++ prototype_nextjs/prisma/seed.ts
@@ -1,6 +1,7 @@
 import { PrismaClient, type Prisma } from '@prisma/client';
 
 import { hashPassword } from '../src/lib/user';
+import { range } from '../src/utils';
 
 const prisma = new PrismaClient();
 
@@ -36,6 +37,28 @@ const getUserData = async (): Promise<Prisma.UserCreateInput[]> => [
     },
   },
   {
+    email: 'manager@make-it-all.co.uk',
+    hashedPassword: await testPassword,
+    name: 'Manager',
+    avatar: { create: {} },
+    inviter: {
+      connect: {
+        email: 'admin@make-it-all.co.uk',
+      },
+    },
+  },
+  {
+    email: 'leader@make-it-all.co.uk',
+    hashedPassword: await testPassword,
+    name: 'Leader',
+    avatar: { create: {} },
+    inviter: {
+      connect: {
+        email: 'admin@make-it-all.co.uk',
+      },
+    },
+  },
+  {
     email: 'left@make-it-all.co.uk',
     hashedPassword: await testPassword,
     name: 'Left The Company',
@@ -48,6 +71,39 @@ const getUserData = async (): Promise<Prisma.UserCreateInput[]> => [
     leftCompany: true,
   },
 ];
+
+const projectData = range(1, 10).map<Prisma.ProjectCreateInput>((num) => ({
+  name: `Project ${num}`,
+  leader: {
+    connect: {
+      email: 'manager@make-it-all.co.uk',
+    },
+  },
+  manager: {
+    connect: {
+      email: 'leader@make-it-all.co.uk',
+    },
+  },
+  members: {
+    create: [
+      {
+        member: {
+          connect: {
+            email: 'alice@make-it-all.co.uk',
+          },
+        },
+      },
+      {
+        member: {
+          connect: {
+            email: 'jane@make-it-all.co.uk',
+          },
+        },
+      },
+    ],
+  },
+}));
+
 
 async function main() {
   console.log('Start seeding ...');
@@ -64,6 +120,13 @@ async function main() {
       },
     });
     console.log(`Created user with id: ${user.id} (name: ${user.name}), invited by email: ${user.inviter?.email}`);
+  }
+
+  for (const p of projectData) {
+    const project = await prisma.project.create({
+      data: p,
+    });
+    console.log(`Created project with id: ${project.id} (name: ${project.name})`);
   }
 
   console.log('Seeding finished.');
