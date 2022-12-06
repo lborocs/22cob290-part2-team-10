d5baa984192c51df456e7bf8bafb26db540889d3
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-06 08:24:28 +0000

    Implement using JWT for invite token

--- prototype_nextjs/src/lib/inviteToken.ts
+++ prototype_nextjs/src/lib/inviteToken.ts
@@ -1,16 +1,43 @@
-// TODO: implement JWT
-// or maybe a different method that gives a shorter URL
-// (compression algorithm?)
+import jwt from 'jsonwebtoken';
 
-let count = 0;
+// use JWT to generate an invite token that contains the inviter's email as payload
+// and expires a week from when it is generated
 
 /**
- * Returns a JWT token that is to be used as as invite token
+ * Returns a JWT token that is to be used as as invite token.
+ *  - can only be used once
+ *  - expires in a week from the date it was generated
  *
+ * @note server-side only
  * @param email Inviter email
  * @returns JWT invite token
  */
 export function getInviteToken(email: string): string {
-  count++;
-  return `token-${count}-{${email.substring(0, email.indexOf('@'))}}`;
+  const secret = process.env.INVITETOKEN_SECRET as string;
+
+  return jwt.sign({
+    email,
+  }, secret, {
+    expiresIn: '20s',
+  });
+}
+
+/**
+ * Decodes the provided token.
+ *
+ * @note server-side only
+ * @param token
+ * @returns The email contained in the token payload
+ */
+export function getEmailFromToken(token: string): string | null {
+  const secret = process.env.INVITETOKEN_SECRET as string;
+
+  try {
+    const decoded = jwt.verify(token, secret) as jwt.JwtPayload;
+    return decoded.email;
+  } catch (err) {
+    // could be one of: expired, incorrect secret, etc.
+    console.error(err);
+    return null;
+  }
 }
