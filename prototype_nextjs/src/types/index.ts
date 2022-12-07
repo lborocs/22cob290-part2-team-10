87e9b2a3be88ea6fa2b50da0e9caafa6a0bd0321
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-07 07:15:23 +0000

    Setup AppPage

--- prototype_nextjs/src/types/index.ts
+++ prototype_nextjs/src/types/index.ts
@@ -1,5 +1,22 @@
+import type { NextPage } from 'next';
 import type { User } from 'next-auth';
 import type { Prisma } from '@prisma/client';
+
+import type { PageLayout } from '~/components/Layout';
+
+/**
+ * Workaround so we don't have to use `Layout` in every page.
+ *
+ * Works by adding these properties to the default export (`Component` in `App`)
+ *  of the page.
+ *
+ * @note using the layout requires `noAuth` to be falsy
+ * @see README # Part 2 # How We Need To Code # Layout/Sidebar
+ */
+export type AppPage<Props = any> = NextPage<Props> & {
+  noAuth?: boolean
+  layout?: PageLayout
+};
 
 export interface SessionUser extends User {
   id: string
