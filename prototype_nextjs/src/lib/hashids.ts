9270ec29aed69a038e901ae2e2a92808da37c146
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-08 13:37:50 +0000

    Implement hashids with strings

--- prototype_nextjs/src/lib/hashids.ts
+++ prototype_nextjs/src/lib/hashids.ts
@@ -8,3 +8,20 @@ const PEPPER = 'Team-10-Group-Project-Part-2';
  */
 const hashids = new Hashids(PEPPER, 6);
 export default hashids;
+
+// hashids with strings: https://stackoverflow.com/a/27137224
+export function encodeString(str: string): string {
+  const hex = Buffer.from(str, 'utf8').toString('hex');
+
+  const encoded = hashids.encodeHex(hex);
+
+  return encoded;
+}
+
+export function decodeString(encoded: string): string {
+  const decodedHex = hashids.decodeHex(encoded);
+
+  const decoded = Buffer.from(decodedHex, 'hex').toString('utf8');
+
+  return decoded;
+}
