dbd8d557971ba1b8fdb82a75867082f0a8746592
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-12 19:40:33 +0000

    Add user_todo sqlite migration (MySQL TODO)

---
+++ prototype_nextjs/prisma/sqlite_migrations/20221212193822_user_todo/migration.sql
@@ -0,0 +1,54 @@
+/*
+  Warnings:
+
+  - Added the required column `deadline` to the `ProjectTask` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "UserTask" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "stage" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "deadline" DATETIME NOT NULL,
+    CONSTRAINT "UserTask_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "UserTaskTags" (
+    "name" TEXT NOT NULL PRIMARY KEY
+);
+
+-- CreateTable
+CREATE TABLE "_UserTaskToUserTaskTags" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL,
+    CONSTRAINT "_UserTaskToUserTaskTags_A_fkey" FOREIGN KEY ("A") REFERENCES "UserTask" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_UserTaskToUserTaskTags_B_fkey" FOREIGN KEY ("B") REFERENCES "UserTaskTags" ("name") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_ProjectTask" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "projectId" INTEGER NOT NULL,
+    "stage" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "deadline" DATETIME NOT NULL,
+    "assigneeId" TEXT NOT NULL,
+    CONSTRAINT "ProjectTask_projectId_fkey" FOREIGN KEY ("projectId") REFERENCES "Project" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "ProjectTask_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_ProjectTask" ("assigneeId", "description", "id", "projectId", "stage", "title") SELECT "assigneeId", "description", "id", "projectId", "stage", "title" FROM "ProjectTask";
+DROP TABLE "ProjectTask";
+ALTER TABLE "new_ProjectTask" RENAME TO "ProjectTask";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserTaskToUserTaskTags_AB_unique" ON "_UserTaskToUserTaskTags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserTaskToUserTaskTags_B_index" ON "_UserTaskToUserTaskTags"("B");
