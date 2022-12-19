bf3e79a2a1fe31974aec6042b43a0b291947254f
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-19 17:38:10 +0000

    Use MUI in examples (WIP)

--- prototype_nextjs/src/pages/examples/index.tsx
+++ prototype_nextjs/src/pages/examples/index.tsx
@@ -1,17 +1,90 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import ListGroup from 'react-bootstrap/ListGroup';
+import List from '@mui/material/List';
+import ListSubheader from '@mui/material/ListSubheader';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+import { NextLinkComposed } from '~/components/Link';
+import ThemeSwitcher from '~/components/layout/ThemeSwitcher';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function ExamplesPage() {
   return (
-    <main className="vh-100 d-flex align-items-center justify-content-center flex-column">
+    <Stack
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      component="main"
+    >
       <Head>
         <title>Examples</title>
       </Head>
 
-      <h1>Examples</h1>
-      <small>{'Note: You need to be signed in (it\'ll redirect you)'}</small>
+      <ThemeSwitcher />
 
+      <Typography variant="h3" component="h1">
+        Examples
+      </Typography>
+      <Typography variant="caption">
+        {'Note: You need to be signed in (it\'ll redirect you to the sign in page)'}
+      </Typography>
+
+      <Paper
+        elevation={3}
+        sx={(theme) => ({
+          bgcolor: theme.palette.mode === 'dark' ? undefined : 'white',
+          marginTop: 2,
+        })}
+      >
+        <List>
+          <section>
+            <List
+              subheader={
+                <ListSubheader
+                  sx={(theme) => ({
+                    bgcolor: theme.palette.mode === 'dark' ? undefined : 'white',
+                  })}
+                >
+                  <Typography variant="h4" component="h2">
+                    Template
+                  </Typography>
+                </ListSubheader>
+              }
+            >
+              <ListItem
+                sx={(theme) => ({
+                  display: 'list-item',
+                  listStyleType: 'disc',
+                  ml: 4,
+                  pl: 0,
+                  width: `calc(100% - ${theme.spacing(4)})`,
+                })}
+              >
+                <ListItemButton
+                  component={NextLinkComposed}
+                  to="/examples/page_template"
+                >
+                  <ListItemText>Page template (you copy and paste the code)</ListItemText>
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </section>
+          <section>
+            {/* TODO: Page section */}
+          </section>
+          <section>
+            {/* TODO: Sidebar section */}
+          </section>
+        </List>
+
+      </Paper >
       <ListGroup as="ul">
         <ListGroup.Item as="li">
           <section>
@@ -55,8 +128,7 @@ export default function ExamplesPage() {
           </section>
         </ListGroup.Item>
       </ListGroup>
-
-    </main>
+    </Stack>
   );
 }
 
