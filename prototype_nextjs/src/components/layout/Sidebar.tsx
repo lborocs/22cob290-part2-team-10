7df9d754069f908f524a238db571b1bc71338794
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-19 17:39:15 +0000

    Override theme in Sidebar (for now)

--- prototype_nextjs/src/components/layout/Sidebar.tsx
+++ prototype_nextjs/src/components/layout/Sidebar.tsx
@@ -1,6 +1,10 @@
+import { useMemo } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { deepmerge } from '@mui/utils';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+import { commonThemeOptions, lightThemeOptions } from '~/pages/_app';
 import styles from '~/styles/layout/Sidebar.module.css';
 import makeItAllLogo from '~/../public/company-logo.png';
 
@@ -10,22 +14,34 @@ export type SidebarProps = {
 };
 
 export default function Sidebar({ show, content }: SidebarProps) {
-  return (
-    <aside className={`${styles.sidebar} ${show ? '' : styles.hidden}`} >
-      <div className={styles.sidebarHeader}>
-        <Link href="/home">
-          <Image
-            src={makeItAllLogo}
-            alt="Company logo"
-            className={styles.companyLogo}
-            priority
-          />
-        </Link>
-      </div>
+  // override app theme for sidebar - at least until configured sidebar colours in dark mode
+  const theme = useMemo(
+    () => createTheme(
+      deepmerge(
+        commonThemeOptions, lightThemeOptions
+      )
+    ),
+    []
+  );
 
-      <div className={styles.sidebarContent}>
-        {content}
-      </div>
-    </aside>
+  return (
+    <ThemeProvider theme={theme}>
+      <aside className={`${styles.sidebar} ${show ? '' : styles.hidden}`} >
+        <div className={styles.sidebarHeader}>
+          <Link href="/home">
+            <Image
+              src={makeItAllLogo}
+              alt="Company logo"
+              className={styles.companyLogo}
+              priority
+            />
+          </Link>
+        </div>
+
+        <div className={styles.sidebarContent}>
+          {content}
+        </div>
+      </aside>
+    </ThemeProvider>
   );
 }
