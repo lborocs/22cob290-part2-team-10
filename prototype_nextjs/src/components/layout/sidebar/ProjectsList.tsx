8a262a2ee191d32099882f606507a7aa74189727
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2022-12-22 03:08:54 +0000

    Extract Projects out from ProjectLists for better content streaming

--- prototype_nextjs/src/components/layout/sidebar/ProjectsList.tsx
+++ prototype_nextjs/src/components/layout/sidebar/ProjectsList.tsx
@@ -1,11 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
+import Alert from '@mui/material/Alert';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import CircularProgress from '@mui/material/CircularProgress';
+import Divider from '@mui/material/Divider';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import Skeleton from '@mui/material/Skeleton';
-import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
@@ -13,85 +18,11 @@ import _ from 'lodash';
 import useSWR from 'swr';
 
 import hashids from '~/lib/hashids';
-import LoadingPage from '~/components/LoadingPage';
+import DebouncedTextField from '~/components/DebouncedTextField';
 import { NextLinkComposed } from '~/components/Link';
 import type { ResponseSchema as GetProjectsResponse } from '~/pages/api/projects/get-assigned-projects';
 
 import styles from '~/styles/layout/sidebar/ProjectsList.module.css';
-
-// TODO: update styling
-// TODO: style scrollbar
-
-// TODO: use Skeleton while loading
-
-const DEBOUNCE_TIMEOUT = 600;
-
-// https://mui.com/material-ui/react-skeleton/
-
-/**
- * Using debouncing for better performance.
- *
- * @param onSearchSubmit
- * @param resetResults
- * @see https://javascript.plainenglish.io/how-to-create-an-optimized-real-time-search-with-react-6dd4026f4fa9
- */
-function SearchBar({ onSearchSubmit, resetResults }: {
-  onSearchSubmit(query: string): void
-  resetResults(): void
-}) {
-  const [query, setQuery] = useState('');
-  const [debouncedQuery, setDebouncedQuery] = useState(query);
-
-  // update `query` after `DEBOUNCE_TIMEOUT`ms from last update of `debouncedQuery`
-  // i.e. `DEBOUNCE_TIMEOUT`ms after user stopped typing
-  useEffect(() => {
-    const timer = setTimeout(() => setQuery(debouncedQuery), DEBOUNCE_TIMEOUT);
-    return () => clearTimeout(timer);
-  }, [debouncedQuery]);
-
-  useEffect(() => {
-    if (query) {
-      onSearchSubmit(query);
-    } else {
-      resetResults();
-    }
-  }, [query, onSearchSubmit, resetResults]);
-
-  return (
-    <TextField
-      id="query"
-      label="Search by name"
-      variant="outlined"
-      value={debouncedQuery}
-      onChange={(e) => setDebouncedQuery(e.target.value)}
-      sx={{
-        marginLeft: 0.5,
-        marginRight: 1.5,
-        my: 1,
-        '& label': {
-          color: 'black',
-        },
-        '& label.Mui-focused': {
-          color: 'black',
-        },
-        '& input': {
-          color: 'black',
-        },
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': {
-            borderColor: 'dark.main',
-          },
-          '&:hover fieldset': {
-            borderColor: 'dark.main',
-          },
-          '&.Mui-focused fieldset': {
-            borderColor: 'dark.main',
-          },
-        },
-      }}
-    />
-  );
-}
 
 /**
  * Using memoization for better performance.
@@ -112,43 +43,160 @@ export default function ProjectsList() {
     return data;
   });
 
-  const [filteredProjects, setFilteredProjects] = useState<NonNullable<typeof projects>>([]);
+  type Projects = NonNullable<typeof projects>;
 
-  // TODO: decide what to show on error
-  if (error) return null;
+  const [filteredProjects, setFilteredProjects] = useState<Projects>([]);
 
-  // TODO: show skeleton
-  if (!projects) return (
-    <LoadingPage dark={false} />
+  const onSearchSubmit = useCallback(
+    (query: string) => projects && setFilteredProjects(filterProjects(query, projects)),
+    [projects]
   );
 
-  const onSearchSubmit = (query: string) => setFilteredProjects(filterProjects(query, projects));
+  const resetResults = useCallback(
+    () => projects && setFilteredProjects(projects),
+    [projects]
+  );
 
-  const resetResults = () => setFilteredProjects(projects);
+  if (error) {
+    console.error(error);
+    return (
+      <Stack height="100%" justifyContent="center">
+        <Alert
+          variant="filled"
+          severity="error"
+          sx={{
+            marginX: 0.25,
+          }}
+        >
+          Failed to load your assigned projects, please try again.
+        </Alert>
+      </Stack>
+    );
+  }
 
-  // TODO: look into using react-window (virtualized list)
+  return (
+    <Stack height="100%" paddingTop={1}>
+      <DebouncedTextField
+        debounceTimeoutMs={600}
+        onSearchSubmit={onSearchSubmit}
+        resetResults={resetResults}
+        id="query"
+        label="Search by project name"
+        variant="outlined"
+        disabled={projects === undefined}
+        sx={{
+          marginLeft: 0.5,
+          marginRight: 1.5,
+          marginTop: 1,
+          '& label': {
+            color: 'black',
+          },
+          '& label.Mui-focused': {
+            color: 'black',
+          },
+          '& input': {
+            color: 'black',
+          },
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderColor: 'dark.main',
+            },
+            '&:hover fieldset': {
+              borderColor: 'dark.main',
+            },
+            '&.Mui-focused fieldset': {
+              borderColor: 'dark.main',
+            },
+          },
+        }}
+      />
+
+      {/* for some reason the divider's lines are in line with the bottom of
+      its content instead of the center. It appears to be because of the parent's
+      height 100%. Wrapping it in a div works as a workaround
+      */}
+      <Box marginY={2.5}>
+        <Divider
+          role="presentation"
+          sx={{
+            marginLeft: 1,
+            marginRight: 1.5,
+            // divider with content colour: https://stackoverflow.com/a/71083943/17381629
+            '&::before, &::after': {
+              borderColor: 'rgba(0, 0, 0, 0.45)',
+            },
+          }}
+        >
+          <Chip
+            label="Assigned Projects"
+            color="contrast"
+            icon={projects ? undefined : <CircularProgress size="1rem" />}
+          />
+        </Divider>
+      </Box>
+
+      <Box position="relative" height="100%" marginRight={0.1}>
+        <Box
+          position="absolute"
+          sx={{
+            inset: 0,
+            overflowY: 'auto',
+            // TODO?: scrollbar styling isnt fully supported on firefox
+            // so try and figure out workaround? idk
+            // https://developer.mozilla.org/en-US/docs/Web/CSS/::-webkit-scrollbar
+            '&::-webkit-scrollbar': {
+              width: '0.5em',
+            },
+            '&::-webkit-scrollbar-track': {
+            },
+            '&::-webkit-scrollbar-thumb': {
+              backgroundColor: 'rgba(0, 0, 0, .1)',
+              outline: '1px solid slategrey',
+            },
+          }}
+        >
+          <Projects projects={projects && filteredProjects} />
+        </Box>
+      </Box>
+    </Stack>
+  );
+}
+
+// TODO?: made list bg a different colour?
+function Projects({ projects }: {
+  projects: GetProjectsResponse | undefined
+}) {
+  if (!projects) return (
+    <List sx={{
+      height: '100%',
+      overflow: 'hidden',
+      marginX: 0.5,
+    }}>
+      {_.range(10).map((num) => (
+        <Skeleton
+          key={num}
+          width="100%"
+          animation="wave"
+        >
+          <ListItem aria-hidden="true">
+            <ListItemText>
+              this cannot be empty
+            </ListItemText>
+          </ListItem>
+        </Skeleton>
+      ))}
+    </List>
+  );
+
+  // TODO: look into using react-window (virtualized list) for better performance
   // https://mui.com/material-ui/react-list/#virtualized-list
 
   return (
-    <div>
-      <Typography fontSize="large" padding={1}>
-        Assigned Projects:
-      </Typography>
-
-      <SearchBar
-        onSearchSubmit={onSearchSubmit}
-        resetResults={resetResults}
-      />
-
-      <List
-        className={styles.projectsList}
-      // dense
-      >
-        {filteredProjects.map((project) => (
-          <ProjectListItem key={project.id} project={project} />
-        ))}
-      </List>
-    </div>
+    <List>
+      {projects.map((project) => (
+        <ProjectListItem key={project.id} project={project} />
+      ))}
+    </List>
   );
 }
 
@@ -181,7 +229,7 @@ function ProjectListItem({ project }: { project: GetProjectsResponse[number] }) 
   );
 
   return (
-    <ListItem disablePadding disableGutters>
+    <ListItem disablePadding>
       {nameIsTooLong ? (
         <Tooltip
           placement="right"
