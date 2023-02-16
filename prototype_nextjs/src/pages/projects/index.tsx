c2e3995e4ec75da9f9f2021588e1afdbb80282c0
koolskateguy89 <61889617+koolskateguy89@users.noreply.github.com>
2023-02-16 02:35:53 +0000

    Cleanup projects page

--- prototype_nextjs/src/pages/projects/index.tsx
+++ prototype_nextjs/src/pages/projects/index.tsx
@@ -2,37 +2,34 @@ import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from 'next';
-
 import Head from 'next/head';
 import { unstable_getServerSession } from 'next-auth/next';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
 
 import { SidebarType } from '~/components/Layout';
 import type { AppPage, SessionUser } from '~/types';
 import { authOptions } from '~/pages/api/auth/[...nextauth]';
-import { PrismaClient, Project, User } from '@prisma/client';
-
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Container,
-  Grid,
-  Link,
-  Paper,
-  Tooltip,
-  Typography,
-} from '@mui/material';
+import prisma from '~/lib/prisma';
 import hashids from '~/lib/hashids';
+import { NextLinkComposed } from '~/components/Link';
 
 const ProjectsPage: AppPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ data, user }) => {
+  // TODO: filtering should be server side
   function hasProjectAccess(
     //A function to filter out the projects that the current user
     //has access to. If the user is either the leader or a member of the project,
     //then they have access to it.
-    project: Project & { leader: User; members: User[] }
+    project: (typeof data)[number]
   ) {
     if (user.id == project.leaderId) {
       return true;
@@ -45,6 +42,8 @@ const ProjectsPage: AppPage<
     return false;
   }
 
+  // TODO: make this a component, it's basically being used as one
+  // but react is currently treating it as a hook
   function hasNoProjects() {
     //function used to display a card that shows the user that they have no projects
     //assigned to them. The Card is only returned if the user has no projects
@@ -76,9 +75,9 @@ const ProjectsPage: AppPage<
           })}
         >
           <Typography
-            fontSize={2 + 'rem'}
-            fontWeight={'lighter'}
-            textAlign={'center'}
+            fontSize="2rem"
+            fontWeight="lighter"
+            textAlign="center"
             color="contrast.main"
           >
             You do not have any projects assigned...
@@ -106,6 +105,7 @@ const ProjectsPage: AppPage<
         <Container sx={{ py: 5 }} maxWidth="md">
           <Grid container spacing={4}>
             {data
+              // TODO: filtering should be done server side
               .filter((project) => hasProjectAccess(project)) /// done to filter out the projects that the current employee does not have access to
               .map((project) => (
                 <Grid item key={project.id} xs={12} sm={6} md={4}>
@@ -124,21 +124,23 @@ const ProjectsPage: AppPage<
                         <Typography gutterBottom variant="h5" component="h2">
                           {project.name.length < 12
                             ? project.name
-                            : project.name.slice(0, 12) + '...'}
+                            : `${project.name.slice(0, 12)}...`}
                         </Typography>
                         <Typography>
                           Project led by: {project.leader.name}
                         </Typography>
                         <Typography>
-                          People working on prject: {project.members.length}
+                          People working on project: {project.members.length}
                         </Typography>
                       </CardContent>
                       <CardActions>
-                        <Link href={`/projects/${hashids.encode(project.id)}`}>
-                          <Button size="small" id={project.id}>
-                            View
-                          </Button>
-                        </Link>
+                        <Button
+                          size="small"
+                          component={NextLinkComposed}
+                          to={`/projects/${hashids.encode(project.id)}`}
+                        >
+                          View
+                        </Button>
                       </CardActions>
                     </Card>
                   </Tooltip>
@@ -170,8 +172,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   }
 
   const user = session.user as SessionUser;
-
-  const prisma = new PrismaClient();
 
   const projects = await prisma.project.findMany({
     // used to query the database for the projects and the members assigned to these projects
